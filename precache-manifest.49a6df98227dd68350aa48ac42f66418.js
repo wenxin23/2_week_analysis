ae2f2a034ef1c4cad4f17b61073da30d7c7595a8
wenxin23 <wenxin23@live.unc.edu>
2020-07-24 11:07:22 -0400

    Updates

---
+++ precache-manifest.49a6df98227dd68350aa48ac42f66418.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74c550d699fbdacfc78088870afdb227",
+    "url": "/2_week_analysis/index.html"
+  },
+  {
+    "revision": "5eaebc9f0c1c8d566765",
+    "url": "/2_week_analysis/static/css/2.4c4477be.chunk.css"
+  },
+  {
+    "revision": "99cc85619e29da76fe53",
+    "url": "/2_week_analysis/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5eaebc9f0c1c8d566765",
+    "url": "/2_week_analysis/static/js/2.be16b595.chunk.js"
+  },
+  {
+    "revision": "cc86dfe93bf93aafdfde67501a8e3898",
+    "url": "/2_week_analysis/static/js/2.be16b595.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99cc85619e29da76fe53",
+    "url": "/2_week_analysis/static/js/main.88f146da.chunk.js"
+  },
+  {
+    "revision": "800c68bfeec4140495f1",
+    "url": "/2_week_analysis/static/js/runtime-main.ad93b840.js"
+  },
+  {
+    "revision": "b121c030adb81eaeb282d2eec3027b44",
+    "url": "/2_week_analysis/static/media/API.b121c030.png"
+  },
+  {
+    "revision": "6c011a25e1739339edfd9f2f40750dcf",
+    "url": "/2_week_analysis/static/media/google.6c011a25.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/2_week_analysis/static/media/logo.5d5d9eef.svg"
+  }
+]);
